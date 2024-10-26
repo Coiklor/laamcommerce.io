5339c3bb6b31a59468c7b047d3706d26236b6b38
coiklor <abraham130998@gmail.com>
2024-10-26 17:29:28 -0400

    few changes

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
-    carItem.classList.remove('active');
+    /* carItem.classList.remove('active') */;
 }
 
 /* let carItem = document.querySelector('.cart-items-container');
